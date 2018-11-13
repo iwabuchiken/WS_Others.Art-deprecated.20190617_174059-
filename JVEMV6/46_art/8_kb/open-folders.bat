bd726e489c99d4f111930635f42fad7e59aee074
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-11-13 17:51:06 +0900

    myself music / kb / admin / write-out / 20181113
    write-out
    ------
    name （タイトル）	TR-87.1-note-multi-hits~LR.no-1
    sub （サブタイトル）	2018-11-11_18-37-05_000.jpg R=3 (20181112_130542)
    domino （作曲者） 	m=20181111_174347.R=3.dms
    score id （作詞者） 	58
    
    name （タイトル）	free-93.flow.no-1
    sub （サブタイトル）	2018-11-12_18-46-01_000.jpg R=1 (20181113_164508)
    domino （作曲者） 	m=20181112_181410.R=1.dms
    score id （作詞者） 	59
    
            ==> done

---
+++ JVEMV6/46_art/8_kb/open-folders.bat
@@ -0,0 +1,1 @@
+start C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
--- JVEMV6/46_art/8_kb/start_write-out.bat
+++ JVEMV6/46_art/8_kb/start_write-out.bat
@@ -1,14 +1,18 @@
 REM start C:\WORKS_2\Programs\musescore\musescore_2.2.1\bin\MuseScore.exe
 
+REM *******************************
+REM 	command line
+REM *******************************
 start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\start_command_prompot.bat
 
+REM *******************************
+REM 	open folders
+REM *******************************
+start open-folders.bat
+
+REM *******************************
+REM 	apps
+REM *******************************
 REM call C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\start_write-out.apps.bat
 start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\start_write-out.apps.bat
 
-REM call C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\start_command_prompot.bat
-
-REM call C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\start_write-out.apps.bat
-
-REM start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\domino\memos
-
-REM start "C:\WORKS_2\Programs\FileZilla FTP Client\filezilla.exe"
