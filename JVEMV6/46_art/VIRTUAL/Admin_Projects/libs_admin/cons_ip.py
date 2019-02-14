d2037f397c7220e89ac7508fc1615ce5412e28fd
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-02-15 07:21:24 +0900

    (Cake_IFM11) periodical updates

--- JVEMV6/46_art/VIRTUAL/Admin_Projects/libs_admin/cons_ip.py
+++ JVEMV6/46_art/VIRTUAL/Admin_Projects/libs_admin/cons_ip.py
@@ -340,7 +340,8 @@ class ColorThresholds(Enum):
     #isRed_HSV_Variance__Lower = 0.002
 #     isRed_HSV_Variance__Lower = 0.01
     
-    isRed_HSV_Average__Upper = 124.0
+    #isRed_HSV_Average__Upper = 124.0	# 20190215_070409
+    isRed_HSV_Average__Upper = 124.6
     isRed_HSV_Average__Lower = 115.0
     #isRed_HSV_Average__Lower = 116.0
 #     isRed_HSV_Average__Lower = 120.0
