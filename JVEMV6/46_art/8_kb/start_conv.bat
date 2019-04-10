573d08083e7c5e7b9bb40db8980976ae6180ec30
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-04-10 10:46:54 +0900

    myself] res free# JVEMV6 52#7 13 / 52. 自然観察 n.o. / 7. 鳥 birds / 20190410
    play
    ------------
    g c -a
    
    # TASK-1 : conpilation
    	==> comp
    
    # Changes to be committed:
    #	new file:   JVEMV6/46_art/8_kb/start_conv.bat

---
+++ JVEMV6/46_art/8_kb/start_conv.bat
@@ -0,0 +1,35 @@
+REM *******************************
+REM 	open folders
+REM *******************************
+echo start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\domino\memos
+start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\domino\memos
+
+REM *******************************
+REM 	apps : itunes
+REM *******************************
+REM call C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\start_write-out.apps.bat
+echo start C:\WORKS_2\Programs\itunes_12.6.2\iTunes.exe
+start C:\WORKS_2\Programs\itunes_12.6.2\iTunes.exe
+
+REM *******************************
+REM 	browser
+REM *******************************
+echo start chrome.exe "https://www.online-convert.com/"
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+REM start chrome.exe "https://www.online-convert.com/"
+start chrome.exe "https://audio.online-convert.com/convert-to-mp3"
+start chrome.exe "https://audio.online-convert.com/convert-to-mp3"
+start chrome.exe "https://audio.online-convert.com/convert-to-mp3"
+
+REM *******************************
+REM 	git pull
+REM *******************************
+REM start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\8_kb\start_command_prompot.bat
+echo git pull....
+pushd C:\WORKS_2\WS\WS_Others.Art
+e
+
+
+REM pause
+
+REM exit
